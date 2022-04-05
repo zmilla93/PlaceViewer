4159e44df7fcc8efd6d7984648bfbcc44e23b62f
Zach Miller <zmilla93@gmail.com>
2022-04-05 01:16:01 -0500

    main frame refactor

--- src/main/java/com/zrmiller/gui/MainFrame.java
+++ src/main/java/com/zrmiller/gui/MainFrame.java
@@ -1,7 +1,6 @@
 package com.zrmiller.gui;
 
 import com.zrmiller.App;
-import com.zrmiller.core.utility.ZUtil;
 
 import javax.swing.*;
 import java.awt.*;
@@ -10,29 +9,18 @@ import java.awt.event.KeyEvent;
 
 public class MainFrame extends JFrame {
 
-    private Container container = getContentPane();
+    private final Container container = getContentPane();
 
     public MainFrame() {
         super("PlaceViewer");
         setIconImage(App.APP_ICON.getImage());
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(1200, 1000);
         container.setLayout(new BorderLayout());
-        CanvasPanel canvasPanel = new CanvasPanel();
-//        container.add(new JButton(), BorderLayout.NORTH);
-        container.add(canvasPanel, BorderLayout.CENTER);
-        setVisible(true);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        JPanel sidebarContainer = new JPanel(new BorderLayout());
-        JPanel sidebar = new JPanel(new GridBagLayout());
-        JButton runButton = new JButton("Rerun shit");
-        GridBagConstraints gc = ZUtil.getGC();
-        sidebar.add(Box.createHorizontalStrut(150), gc);
-        gc.gridy++;
-        gc.fill = GridBagConstraints.HORIZONTAL;
-        sidebar.add(runButton, gc);
-        gc.gridy++;
-        container.setFocusable(true);
+        WindowContainer windowContainer = new WindowContainer();
+        container.add(windowContainer, BorderLayout.CENTER);
+
         container.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
@@ -40,10 +28,7 @@ public class MainFrame extends JFrame {
             }
         });
 
-        sidebarContainer.add(sidebar, BorderLayout.NORTH);
-        container.add(sidebarContainer, BorderLayout.WEST);
-
-
+        setVisible(true);
     }
 
 }
--- src/main/java/com/zrmiller/gui/WindowContainer.java
+++ src/main/java/com/zrmiller/gui/WindowContainer.java
@@ -1,4 +1,31 @@
 package com.zrmiller.gui;
 
-public class WindowContainer {
+import javax.swing.*;
+import java.awt.*;
+
+public class WindowContainer extends JPanel {
+
+    private CanvasPanel canvasPanel = new CanvasPanel();
+    private JPanel sidebarPanel = new JPanel(new GridBagLayout());
+    private JPanel southPanel = new JPanel(new GridBagLayout());
+    private JLabel frameLabel = new JLabel("Unloaded");
+
+    public WindowContainer(){
+        setLayout(new BorderLayout());
+        // Panels
+        buildPanels();
+        addPanels();
+    }
+
+    private void buildPanels(){
+        sidebarPanel.add(new JButton("ASJFKLSDJAF"));
+//        southPanel.setBorder();
+    }
+
+    private void addPanels(){
+        add(sidebarPanel, BorderLayout.WEST);
+        add(canvasPanel, BorderLayout.CENTER);
+        add(southPanel, BorderLayout.SOUTH);
+    }
+
 }
