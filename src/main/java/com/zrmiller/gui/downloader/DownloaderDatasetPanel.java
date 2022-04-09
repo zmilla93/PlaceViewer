d5b7200d83cd6dc3a9879675c8cf9a55430fa128
Zach Miller <zmilla93@gmail.com>
2022-04-09 00:50:58 -0500

    fixme

--- src/main/java/com/zrmiller/gui/downloader/DownloaderDatasetPanel.java
+++ src/main/java/com/zrmiller/gui/downloader/DownloaderDatasetPanel.java
@@ -7,6 +7,7 @@ import java.awt.*;
 
 public abstract class DownloaderDatasetPanel extends BaseDownloaderPanel {
 
+    // FIXME: Merge with BaseDownloaderPanel
     protected final DatasetManagerFrame datasetManagerFrame;
     protected final CardLayout cardLayout = new CardLayout();
     protected final JPanel cardPanel = new JPanel(cardLayout);
