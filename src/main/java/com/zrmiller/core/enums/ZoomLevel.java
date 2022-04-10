b422f1d7226a2e02af5ae2654b5cb0532f0a27dd
Zach Miller <zmilla93@gmail.com>
2022-04-10 18:40:39 -0500

    zoom level stub

---
+++ src/main/java/com/zrmiller/core/enums/ZoomLevel.java
@@ -0,0 +1,27 @@
+package com.zrmiller.core.enums;
+
+public enum ZoomLevel {
+
+    Zoom_25("25%", 4, true),
+    Zoom_50("50%", 2, true),
+    Zoom_100("100%", 1),
+    Zoom_200("200%", 2),
+    Zoom_400("400%", 4),
+    Zoom_800("800%", 8),
+    ;
+
+    public final String name;
+    public final int modifier;
+    public final boolean zoomOut;
+
+    ZoomLevel(String name, int modifier) {
+        this(name, modifier, false);
+    }
+
+    ZoomLevel(String name, int modifier, boolean zoomOut) {
+        this.name = name;
+        this.modifier = modifier;
+        this.zoomOut = zoomOut;
+    }
+
+}
