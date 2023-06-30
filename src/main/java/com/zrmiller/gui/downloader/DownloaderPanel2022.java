169e3200739746d562ad81517a8f05c37b11db75
Zach Miller <zmilla93@gmail.com>
2023-06-29 21:27:14 -0500

    Update DownloaderPanel2022.java

--- src/main/java/com/zrmiller/gui/downloader/DownloaderPanel2022.java
+++ src/main/java/com/zrmiller/gui/downloader/DownloaderPanel2022.java
@@ -1,7 +1,6 @@
 package com.zrmiller.gui.downloader;
 
 import com.zrmiller.App;
-import com.zrmiller.core.datawrangler.DataDownloader2017;
 import com.zrmiller.core.datawrangler.DataDownloader2022;
 import com.zrmiller.core.datawrangler.DataValidator;
 import com.zrmiller.core.datawrangler.callbacks.IValidationListener2022;
@@ -11,8 +10,6 @@ import com.zrmiller.core.utility.ZUtil;
 import com.zrmiller.gui.frames.DatasetManagerFrame;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class DownloaderPanel2022 extends CardDownloaderPanel implements IValidationListener2022 {
 
