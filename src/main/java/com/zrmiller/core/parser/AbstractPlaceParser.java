df88f8e3c19461fb40657b9fba1a1d5f112c0621
Zach Miller <zmilla93@gmail.com>
2023-07-03 22:37:50 -0500

    removed unused code

--- src/main/java/com/zrmiller/core/parser/AbstractPlaceParser.java
+++ src/main/java/com/zrmiller/core/parser/AbstractPlaceParser.java
@@ -2,30 +2,9 @@ package com.zrmiller.core.parser;
 
 import com.zrmiller.core.utility.TileEdit;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 
 public abstract class AbstractPlaceParser {
-
-    // FIXME : Move this to somewhere more logical
-    private static final int META_BYTE_COUNT = 6;
-
-    /**
-     * Opens a file and reads the first 6 bytes.
-     *
-     * @param fullPath
-     * @return
-     * @throws IOException
-     */
-    protected BufferedInputStream openInputStream(String fullPath) throws IOException {
-        BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(fullPath));
-        byte[] meta = new byte[META_BYTE_COUNT];
-        int bytesRead = inputStream.read(meta);
-        // TODO : Validate metadata and throw exception if invalid
-//        if(bytesRead != META_BYTE_COUNT)
-        return inputStream;
-    }
 
     abstract boolean openStream();
 
