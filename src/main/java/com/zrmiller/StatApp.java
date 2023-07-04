961b03ffe83c6137497d714fcbb8fcf620144038
Zach Miller <zmilla93@gmail.com>
2023-07-03 22:43:34 -0500

    Update StatApp.java

--- src/main/java/com/zrmiller/StatApp.java
+++ src/main/java/com/zrmiller/StatApp.java
@@ -3,24 +3,27 @@ package com.zrmiller;
 import com.zrmiller.core.parser.PlaceParser2022;
 
 import java.io.IOException;
+import java.text.NumberFormat;
 
 public class StatApp {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         PlaceParser2022 parser = new PlaceParser2022();
         parser.openStream();
         int i = 0;
         System.out.println("Running...");
-        try{
-            while(parser.ready()){
+        long time = System.currentTimeMillis();
+        try {
+            while (parser.ready()) {
                 parser.readNextLine();
                 i++;
             }
-        } catch(IOException e){
-
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        System.out.println("Tile count:" + i);
-
+        time = System.currentTimeMillis() - time;
+        System.out.println("Tile count: " + NumberFormat.getInstance().format(i));
+        System.out.println("Run time: " + (time / 1000f) + " seconds");
     }
 
 }
